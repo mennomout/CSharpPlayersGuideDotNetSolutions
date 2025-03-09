a3f1314b32149a382e158cb42ebe30c21f8abb9d
Neowis3 <m.menno@outlook.com>
2025-03-09 20:57:15 +0100

    Updated the book to create instances of levels through reflection instead of manual entry of each new level.

--- Book/Book.cs
+++ Book/Book.cs
@@ -1,71 +1,13 @@
 ﻿using Challenges;
-using Challenges.Part_1.Level_10;
-using Challenges.Part_1.Level_11;
-using Challenges.Part_1.Level_12;
-using Challenges.Part_1.Level_13;
-using Challenges.Part_1.Level_14;
-using Challenges.Part_1.Level_8;
-using Challenges.Part_1.Level_9;
-using Challenges.Part_2.Level_25;
-using Challenges.Part_2.Level_15;
-using Challenges.Part_2.Level_16;
-using Challenges.Part_2.Level_17;
-using Challenges.Part_2.Level_18;
-using Challenges.Part_2.Level_19;
-using Challenges.Part_2.Level_20;
-using Challenges.Part_2.Level_21;
-using Challenges.Part_2.Level_22;
-using Challenges.Part_2.Level_23;
-using Challenges.Part_2.Level_24;
-using ChallengesPart1Level1;
-using ChallengesPart1Level2;
-using ChallengesPart1Level3;
-using ChallengesPart1Level4;
-using ChallengesPart1Level5;
-using ChallengesPart1Level6;
-using ChallengesPart1Level7;
 using Helpers;
-using Challenges.Part_2.Level_26;
-using Challenges.Part_2.Level_27;
-using Challenges.Part_2.Level_28;
-using Challenges.Part_2.Level_29;
-using Challenges.Part_2.Level30;
+using System.Reflection;
 
 namespace CSharpPlayersGuideDotNetBook;
 
 public class Book
 {
-    public List<BaseLevel> Levels { get; set; } = [
-        new Level1(),
-        new Level2(),
-        new Level3(),
-        new Level4(),
-        new Level5(),
-        new Level6(),
-        new Level7(),
-        new Level8(),
-        new Level9(),
-        new Level10(),
-        new Level11(),
-        new Level12(),
-        new Level13(),
-        new Level14(),
-        new Level15(),
-        new Level16(),
-        new Level17(),
-        new Level18(),
-        new Level19(),
-        new Level20(),
-        new Level21(),
-        new Level22(),
-        new Level23(),
-        new Level24(),
-        new Level25(),
-        new Level26(),
-        new Level27(),
-        new Level28(),
-        new Level29(),
-        new Level30()];
+    private static readonly IEnumerable<Type>? _levels = Assembly.GetAssembly(typeof(BaseLevel))?.GetTypes().Where(static x => x.BaseType == typeof(BaseLevel));
+    public static List<BaseLevel?>? Levels => _levels?.Select(x => (BaseLevel?)Assembly.GetAssembly(typeof(BaseLevel))?.CreateInstance(x.FullName ?? string.Empty))?.OrderBy(x => x?.Id).ToList();
 
     public void Open()
     {
@@ -121,21 +63,24 @@ public class Book
 
     private Challenge? GetChallenge(string input)
     {
-        return Levels.SelectMany(x => x.Challenges)?.FirstOrDefault(x => x.Name.Equals(input, StringComparison.OrdinalIgnoreCase));
+        return Levels?.SelectMany(x => x.Challenges)?.FirstOrDefault(x => x.Name.Equals(input, StringComparison.OrdinalIgnoreCase));
     }
 
     public void DisplayChallenges()
     {
         Console.WriteLine("Welcome to the C# Players Guide answers by Neowise");
         Console.WriteLine("What Level would you like to open?");
-        
-        foreach (var level in Levels)
-        {
-            Console.WriteLine($"\t{level.Id}. {level.Name}");    
 
-            foreach (var challenge in level.Challenges)
+        foreach (var level in Levels ?? [])
+        {
+            if (level != null)
             {
-                Console.WriteLine($"\t\t*{challenge.Name}");
+                Console.WriteLine($"\t{level.Id}. {level.Name}");
+
+                foreach (var challenge in level.Challenges)
+                {
+                    Console.WriteLine($"\t\t*{challenge.Name}");
+                }
             }
         }
     }
